152ab1ffe060e3d6923812498148a26c07fd4844
horacinis <horacinis@gmail.com>
2020-09-24 23:25:09 +1000

    Created the blueprint for the list of products for coffees, teas and soft drinks

---
+++ test.rb
@@ -0,0 +1,12 @@
+# jefe = {name: 'Horacio'}
+jefe = ['Horacinis']
+
+jefe_class = jefe.class
+
+if jefe_class == Hash
+    puts "It's a Hash"
+elsif jefe_class == Array
+    puts "It's an Array"
+else
+    puts "It is none!"
+end
